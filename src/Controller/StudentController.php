726483bf4ee57d3464e45062cb627ca8a43b9140
minhvqgcc210229 <121932119+minhvqgcc210229@users.noreply.github.com>
2023-02-26 02:57:36 +0700

    update student
    templates index

--- src/Controller/StudentController.php
+++ src/Controller/StudentController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\StudentRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +10,12 @@ use Symfony\Component\Routing\Annotation\Route;
 class StudentController extends AbstractController
 {
     #[Route('/student', name: 'app_student')]
-    public function index(): Response
+    public function index(StudentRepository $repo): Response
     {
+        $student = $repo->findAll();
         return $this->render('student/index.html.twig', [
             'controller_name' => 'StudentController',
+            'student' => $student
         ]);
     }
 }
