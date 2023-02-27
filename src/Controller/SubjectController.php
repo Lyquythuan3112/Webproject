447dfe05b95c0b49d53f9c1b0669b9b87e0cf19d
GiangTuan1 <tuanggcc210137@fpt.edu.vn>
2023-02-27 19:17:58 +0700

    subject form

--- src/Controller/SubjectController.php
+++ src/Controller/SubjectController.php
@@ -2,17 +2,37 @@
 
 namespace App\Controller;
 
+use App\Entity\Subject;
+use App\Form\SubjectType;
+use App\Repository\ClassesRepository;
+use App\Repository\SubjectRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+
+
 class SubjectController extends AbstractController
 {
-    #[Route('/subject', name: 'app_subject')]
-    public function index(): Response
-    {
-        return $this->render('subject/index.html.twig', [
-            'controller_name' => 'SubjectController',
-        ]);
+   
+    
+   /**
+    * @Route("/subject", name="subject_show")
+    */
+    public function createAction(Request $req , SubjectRepository $repo): Response
+   {
+    $d = new Subject();
+    $form = $this->createForm(SubjectType ::class, $d);
+
+    $form->handleRequest($req);
+    if($form->isSubmitted()&&$form->isValid()){
+        $repo->save($d,true);
+        return $this->redirectToRoute('guest', [], Response::HTTP_SEE_OTHER);
+
+    }
+        return $this->render('subject/form.html.twig', [
+    ]);
     }
 }
+
